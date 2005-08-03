6aa1a92f68b86b76e974eb595af8300a920c1de8
jonasbn <jonasbn@logiclab.dk>
2005-08-03 13:21:56 +0000

    Dynamic PHP version
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BOPCC/trunk@5624 e6edf6fb-f266-4316-afb4-e53d95876a76

--- php/url_nok.php
+++ php/url_nok.php
@@ -1,23 +1,29 @@
-<!-- $Id: url_nok.php,v 1.1 2005-08-03 13:05:50 jonasbn Exp $ -->
-<?php echo('<?xml version="1.0" encoding="ISO-8859-1"?>'); ?>
-<CUSTOMER>
-		<CUST_NAME><?php=$cust_name ?></CUST_NAME>
-		<CUST_STREET><?php $cust_street ?></CUST_STREET>
-		<CUST_COMPANY><?php $cust_company ?></CUST_COMPANY>
-		<CUST_ZIP><?php $cust_zip ?></CUST_ZIP>
-		<CUST_CITY><?php $cust_city ?></CUST_CITY>
-		<CUST_STATE><?php $cust_state ?></CUST_STATE>
-		<CUST_COUNTRY><?php $cust_country ?></CUST_COUNTRY>
-		<CUST_PHONE><?php $cust_phone ?></CUST_PHONE>
-		<CUST_FAX><?php $cust_fax ?></CUST_FAX>
-		<CUST_EMAIL><?php $cust_email ?></CUST_EMAIL>
-	</CUSTOMER>
-	<ORDER>
-		<AMOUNT><?php $amount ?></AMOUNT>
-		<ORDERID><?php $orderid ?></ORDERID>
-		<SESSION><?php $sessionid ?></SESSIONID>
-		<ERRORTYPE><?php $errortype ?></ERRORTYPE>
-		<PAYMENTTYPE><?php $paymenttype ?></PAYMENTTYPE>
-		<SUBSCRIBE><?php $subscribe ?></SUBSCRIBE>
-	</ORDER>
+<?php echo('<?XML version="1.0" encoding="ISO-8859-1"?>');
+
+$seek = array(
+	'cust_name',
+	'cust_street',
+	'cust_company',
+	'cust_zip',
+	'cust_city',
+	'cust_state',
+	'cust_coutry',
+	'cust_phone',
+	'cust_fax', 
+	'cust_email',
+	'amount',
+	'orderid',
+	'sessionid',
+	'errortype',
+	'paymenttype',
+	'subscribe'
+);
+while (list($placeholder, $var) = each($seek)) {
+	if (isset($$var)) {
+		$thisval = $$var;
+		$tag = strtoupper($var);
+		print "<$tag>$thisval</$tag>";
+	}
+}
+?>
 </XML>
