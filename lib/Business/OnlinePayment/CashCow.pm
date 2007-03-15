b8091f651a6df1df43a5938f1380c5c58f03bb89
jonasbn <jonasbn@logiclab.dk>
2007-03-15 19:47:20 +0000

    Release candidate 0.07
    git-svn-id: file:///Users/jonasbn/Desktop/svnrepos/local-svn/BOPCC/trunk@5659 e6edf6fb-f266-4316-afb4-e53d95876a76

--- lib/Business/OnlinePayment/CashCow.pm
+++ lib/Business/OnlinePayment/CashCow.pm
@@ -1,6 +1,6 @@
 package Business::OnlinePayment::CashCow;
 
-# $Id: CashCow.pm,v 1.23 2007-03-12 08:05:19 jonasbn Exp $
+# $Id: CashCow.pm,v 1.24 2007-03-15 19:47:20 jonasbn Exp $
 
 use strict;
 use warnings;
@@ -13,7 +13,7 @@ use Data::Dumper;
 
 use constant DEBUG => 0;
 
-$VERSION = '0.06';
+$VERSION = '0.07';
 
 sub set_defaults {
     my $self = shift;
